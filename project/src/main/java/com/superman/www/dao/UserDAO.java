e871041d753a0821c9ebc9afe8489f0dee3437b4
jeongwook <rkdwjddnr11@naver.com>
2020-12-29 22:21:22 +0900

    hobbyC Update

--- project/src/main/java/com/superman/www/dao/UserDAO.java
+++ project/src/main/java/com/superman/www/dao/UserDAO.java
@@ -1,56 +1,7 @@
 package com.superman.www.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.ArrayList;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-
-public class UserDAO {
-
-	private static UserDAO dao = new UserDAO();
-	DataSource dataSource;
-	private final String CONN_POOL_SOURCE = "jdbc/hobbyc";
-	
-	private UserDAO() {
-		try {
-			Context context = new InitialContext();
-			dataSource = (DataSource)context.lookup("java:comp/env/" + CONN_POOL_SOURCE);
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	public static UserDAO getDAO() {
-		return dao;
-	}
-	public Connection getConnection() {
-		Connection conn = null;
-		try {
-			conn = dataSource.getConnection();
-		} catch(SQLException e) {
-			e.printStackTrace();
-		}
-		return conn;
-	}
-	public void close(Connection conn,PreparedStatement pst,ResultSet rs) {
-		try {
-			if(conn != null) conn.close();
-			if(pst != null) pst.close();
-			if(rs != null) rs.close();
-		} catch(SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	public void close(Connection conn,PreparedStatement pst) {
-		try {
-			if(conn != null) conn.close();
-			if(pst != null) pst.close();
-		} catch(SQLException e) {
-			e.printStackTrace();
-		}
-	}
+public interface UserDAO {
 	
 }
