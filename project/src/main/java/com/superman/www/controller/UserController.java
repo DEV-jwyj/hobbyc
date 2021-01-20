d133f838ef2ef195efaed5a15d76a7b0c11e84c7
yeongji kim <76148671+DEV-yeongji@users.noreply.github.com>
2021-01-20 23:47:24 +0900

    mybatis 삭제 jdbc 등록!

--- project/src/main/java/com/superman/www/controller/UserController.java
+++ project/src/main/java/com/superman/www/controller/UserController.java
@@ -1,11 +1,22 @@
 package com.superman.www.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.superman.www.util.Constant;
+
 @Controller
 public class UserController {
-
+public JdbcTemplate template;
+	
+	@Autowired
+	// @Autowired는 주입하려고 하는 객체의 타입이 일치하는 객체를 자동으로 주입한다.
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+		Constant.template = this.template;
+	}
 	@RequestMapping("/join")
 	public String join() {
 		return "join";
